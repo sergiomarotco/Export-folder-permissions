3ae5dd916db2752312f73b85248de64a2e3139d3
Sergio Marotco <sergiomarotco@gmail.com>
2018-02-13 23:22:45 +0700

    Добавьте файлы проекта.

---
+++ FolderRules/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FolderRules
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.linkLabel1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 630);
+            this.splitContainer1.SplitterDistance = 62;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 1);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Сменить папку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 28);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(24, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Глубина поиска:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.Location = new System.Drawing.Point(4, 57);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(166, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Описание прав доступа";
+            this.linkLabel1.Click += new System.EventHandler(this.LinkLabel1_Click);
+            this.linkLabel1.MouseEnter += new System.EventHandler(this.LinkLabel1_MouseEnter);
+            this.linkLabel1.MouseLeave += new System.EventHandler(this.LinkLabel1_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(751, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Узнать права";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(605, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "C:\\1Test";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(869, 563);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(751, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Прервать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 630);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Узнать права на папку";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FolderRules/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FolderRules
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            allow_work = true;
+            button3.Visible = true;
+            richTextBox1.Clear();
+            richTextBox1.Text += "Имя папки,\t,Имя учетной записи,\tРазрешено,\tЗапрещено" + Environment.NewLine;
+            string[] drives = Environment.GetLogicalDrives();
+            Task.Factory.StartNew(() => WalkDirectoryTree(textBox1.Text));
+        }
+        //List<string> filesanddirecoties;
+        //DirectoryInfo[] files;
+        private void WalkDirectoryTree(string RootDirectory)
+        {
+            DirectoryInfo root = new DirectoryInfo(RootDirectory);
+            if (root.Exists)
+            {
+                try
+                {
+                    //filesanddirecoties = new List<string>();
+                    DirectoryInfo[] subDirs = root.GetDirectories();
+                    AuthorizationRuleCollection ACLs = root.GetAccessControl().GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                    string[] Tree = root.FullName.Split('\\');
+
+                    //строим карту папок и файлов
+                    /* for (int i = 0; i < Tree.Length; i++)
+                     {
+                         filesanddirecoties.Add(Tree[i]);
+                         string[] files = Directory.GetFiles(RootDirectory);
+                         for (int j = 0; j < files.Length; j++)
+                         {
+                             if(files.Length!=0)
+                                 filesanddirecoties.Add(files[j]);
+                         }
+                     }*/
+                    //получаем права на папку
+                    foreach (FileSystemAccessRule ACL in ACLs)
+                    {
+                        if (!IsSystemRules(ACL.IdentityReference.ToString()))
+                        {
+                            if (allow_work)
+                            {
+                                if (ACL.AccessControlType.ToString().Equals("Allow"))
+                                {
+                                    Invoke((ThreadStart)delegate { richTextBox1.Text += RootDirectory + ",\t" + ACL.IdentityReference + ",\t" + ACL.FileSystemRights + Environment.NewLine; });
+                                }
+                                else if (ACL.AccessControlType.ToString().Equals("Deny"))
+                                {
+                                    Invoke((ThreadStart)delegate { richTextBox1.Text += RootDirectory + ",\t" + ACL.IdentityReference + ",\t,\t" + ACL.FileSystemRights + Environment.NewLine; });
+                                }
+                            }
+                        }
+                    }
+                  
+                    if (Tree.Length < Convert.ToInt32(textBox2.Text) + 2)
+                    {
+                        foreach (DirectoryInfo dirInfo in subDirs)
+                        {
+                            if (allow_work)
+                                WalkDirectoryTree(dirInfo.FullName);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException ee)
+                {
+                    Invoke((ThreadStart)delegate { richTextBox1.Text += RootDirectory + ",\tAccess Denied" + Environment.NewLine /*+ ee.StackTrace*/; });
+                }
+                catch (Exception ww) { MessageBox.Show(ww.StackTrace); }
+            }
+            else
+            {
+                Invoke((ThreadStart)delegate { richTextBox1.Text = "Папка не существует. Отмена операции."; });
+            }
+        }
+
+        private readonly string[] systemrules = new string[] { "СОЗДАТЕЛЬ-ВЛАДЕЛЕЦ", "BUILTIN", "NT AUTHORITY" };
+        public bool IsSystemRules(string whattochek)
+        {
+            bool IsSystemRule = false;
+            for (int i = 0; i < systemrules.Length; i++)
+            {
+                if (whattochek.Contains(systemrules[i]))
+                {
+                    return IsSystemRule = true;
+                }
+            }
+
+            return IsSystemRule;
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void LinkLabel1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://msdn.microsoft.com/ru-ru/library/system.security.accesscontrol.filesystemrights.aspx");
+        }
+
+        private void LinkLabel1_MouseEnter(object sender, EventArgs e)
+        {
+            linkLabel1.Text = "https://msdn.microsoft.com/ru-ru/library/system.security.accesscontrol.filesystemrights.aspx";
+        }
+
+        private void LinkLabel1_MouseLeave(object sender, EventArgs e)
+        {
+            linkLabel1.Text = "Описание прав доступа";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.DefaultDirectory;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DefaultDirectory = textBox1.Text;
+            Properties.Settings.Default.Upgrade();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            allow_work = false;
+            button3.Visible = false;
+        }
+
+        private bool allow_work = false;
+    }
+}
